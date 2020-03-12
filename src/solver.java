5666302d40e1c0fec6d582e05693af95f3aba346
TiagoSD22 <tiagosdionizio@gmail.com>
2020-03-12 13:00:15 -0300

    modelando project manager

--- src/solver.java
+++ src/solver.java
@@ -32,9 +32,30 @@ public class solver {
         }
     }
 
-    public class CalculateDeveloppersWP(Developer dev){
+    public class ProjectManager{
+        private String company;
+        private int bonusPotential;
 
+        public ProjectManager(String company, int bonusPotential) {
+            this.company = company;
+            this.bonusPotential = bonusPotential;
+        }
+
+        public String getCompany() {
+            return company;
+        }
+
+        public void setCompany(String company) {
+            this.company = company;
+        }
+
+        public int getBonusPotential() {
+            return bonusPotential;
+        }
+
+        public void setBonusPotential(int bonusPotential) {
+            this.bonusPotential = bonusPotential;
+        }
     }
-
 }
 
