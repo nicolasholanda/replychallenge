6471f201e2b7a1bfcc757e98d5488e070aa9ce21
Lucas <lucasmag97@gmail.com>
2020-03-12 12:55:16 -0300

    getter e setters

--- src/solver.java
+++ src/solver.java
@@ -6,7 +6,35 @@ public class solver {
         public String company;
         public int bonusPotential;
         public ArrayList<String> skills;
+
+        public String getCompany() {
+            return company;
+        }
+
+        public void setCompany(String company) {
+            this.company = company;
+        }
+
+        public int getBonusPotential() {
+            return bonusPotential;
+        }
+
+        public void setBonusPotential(int bonusPotential) {
+            this.bonusPotential = bonusPotential;
+        }
+
+        public ArrayList<String> getSkills() {
+            return skills;
+        }
+
+        public void setSkills(ArrayList<String> skills) {
+            this.skills = skills;
+        }
     }
 
+    public class CalculateDeveloppersWP(Developer dev){
+
+    }
 
 }
+
