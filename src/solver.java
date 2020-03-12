938a9d6b44e4e3b4a572602b4f49b40c1735e35b
TiagoSD22 <tiagosdionizio@gmail.com>
2020-03-12 12:52:45 -0300

    modelando developer

--- src/solver.java
+++ src/solver.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+
 public class solver {
+
+    public class Developer{
+        public String company;
+        public int bonusPotential;
+        public ArrayList<String> skills;
+    }
+
 
 }
